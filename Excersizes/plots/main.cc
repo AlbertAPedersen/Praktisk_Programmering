dba30d128832db93b8464cac08bc5b887c3abf7e
Your Name <albertapedersen@gmail.com>
2026-02-18 12:11:11 +0100

    plots

--- Excersizes/plots/main.cc
+++ Excersizes/plots/main.cc
@@ -2,8 +2,10 @@
 #include<iostream>
 #include<cmath>
 #include<vector>
+#include<complex>
 
 const double PI = 3.1415926535;
+const double e  = 2.7182818284590452;
 double erf(double x){
 // single precision error function (Abramowitz and Stegun, from Wikipedia)
 if(x<0) return -erf(-x);
@@ -14,6 +16,7 @@ return 1-sum*std::exp(-x*x);
 }
 
 double sgamma(double x){
+
 if(x<0)return PI/std::sin(PI*x)/sgamma(1-x);
 if(x<9)return sgamma(x+1)/x;
 double lnsgamma=std::log(2*PI)/2+(x-0.5)*std::log(x)-x
@@ -22,9 +25,17 @@ return std::exp(lnsgamma);
 }
 
 
+std::complex<double> cgamma(std::complex<double> z){
+
+    if(z.real()<0.0)return PI/std::sin(PI*z)/cgamma(1.0-z);
+    if(z.real()<9.0)return cgamma(z+1.0)/z;
+    std::complex<double> lncgamma=std::log(2.0*PI)/2.0+(z-0.5)*std::log(z)-z
+        +(1.0/12.0)/z-(1.0/360.0)/(z*z*z)+(1.0/1260.0)/(z*z*z*z*z);
+    return std::exp(lncgamma);
+}
 
 int main(int argc, char** argv) {
-    double xmin=0, xmax=10, dx=0.125;
+    double xmin=-2, xmax=2, dx=0.125, imin=-2, imax=2, di=0.125;
     std::vector<double> x_tab {0.5,1,1.5,2};
     std::vector<double> y_tab_erf {0.520499878,0.842700793,0.966105146,0.995322265};
     std::vector<double> y_tab_sgamma {1.77245,1,0.886227,1};
@@ -35,18 +46,33 @@ int main(int argc, char** argv) {
     if(arg=="-xmin" && i+1<argc) xmin=std::stod(argv[i+1]);
     if(arg=="-xmax" && i+1<argc) xmax=std::stod(argv[i+1]);
     if(arg=="-dx" && i+1<argc) dx=std::stod(argv[i+1]);
+
+    if(arg=="-imin" && i+1<argc) imin=std::stod(argv[i+1]);
+    if(arg=="-imax" && i+1<argc) imax=std::stod(argv[i+1]);
+    if(arg=="-di" && i+1<argc) di=std::stod(argv[i+1]);
+    
     }
 
     std::cout<<std::scientific;
 
     for(double x=xmin;x<xmax;x+=dx){
-        std::cout<< x <<" "<< erf(x) <<" "<<sgamma(x)<<"\n";
+        std::cout<< x <<" "<< erf(x) <<" "<<sgamma(x)<< "\n";
     }
     std::cout<< "\n\n"; //next block for GNU
     
     for(int i=0;i<x_tab.size();i++){
     std::cout<< x_tab[i] << " " << y_tab_erf[i] << " " << y_tab_sgamma[i]<<"\n";
     }
+
+    std::cout<< "\n\n"; //next block for GNU
+    for(double x=xmin;x<xmax;x+=dx){    
+        for (double i=imin;i<imax;i+=di){
+            std::complex<double> Z(x,i);
+        std::cout<< x << " " << i << " " << std::norm(cgamma(Z)) << "\n";
+        }
+        std::cout << "\n";
+    }
+    
 
 
 
