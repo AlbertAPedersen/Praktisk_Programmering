69291733e05ce3176cc75b70f249acecaa93d3ae
Albert Pedersen <Albert@DESKTOP-MH3AG9A.localdomain>
2026-04-07 11:04:00 +0200

    Add ODE

---
+++ homework/ODE/main.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include"../packages/matrix_QR.h"
+//#include<tuple>
+//#include"lsfit.h"
+#include"spline.h"
+#include<assert.h>
+#include<cmath>
+
+
+int main(){
+
+return 0;
+}
